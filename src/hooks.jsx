bf7755bf48997587fee5035b83cc2a5ff305c37c
Scott Waller <ssherwoodw@gmail.com>
2023-09-22 15:24:15 -0700

    adding files

---
+++ src/hooks.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+
+const useFlip = () => {
+    const [isFacingUp, setIsFacingUp] = useState(true);
+    const flipCard = () => {
+    setIsFacingUp(isUp => !isUp);
+  };
+    
+    // return piece of state and toggle function
+    return [isFacingUp, flipCard];
+}
+
+function useAxios(keyInLS, baseUrl) {
+  const [responses, setResponses] = useLocalStorage(keyInLS);
+
+  const addResponseData = async (formatter = data => data, restOfUrl = "") => {
+    const response = await axios.get(`${baseUrl}${restOfUrl}`);
+    setResponses(data => [...data, formatter(response.data)]);
+  };
+
+  const clearResponses = () => setResponses([]);
+
+  return [responses, addResponseData, clearResponses];
+}
+
+function useLocalStorage(key, initialValue = []) {
+  if (localStorage.getItem(key)) {
+    initialValue = JSON.parse(localStorage.getItem(key));
+  }
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+}
+
+export default useLocalStorage;
+
+export { useFlip, useAxios, useLocalStorage };
---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
